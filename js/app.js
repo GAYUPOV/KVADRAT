cd6537ab1bec2ef6934dee129ff3cb6bfa420344
Bekpolat <bgayupov02@gmail.com>
2022-04-06 19:48:24 +0300

    KVADRAT FULL

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let clientUserName = prompt("Assalomu Aleykum, Iltimos Ismingizni Kiriting");
+while (!isNaN(clientUserName) || clientUserName == "") {
+    clientUserName = prompt("Assalomu Aleykum, Iltimos Faqat Ismingizni Kiriting");
+}
+let clientCount = +prompt(clientUserName + " Son Kiriting");
+while (isNaN(clientCount) || clientCount == "") {
+    clientCount = +prompt(clientUserName + " Faqat Son kiriting");
+}
+let result = "";
+for (let i = 0; i < clientCount; i++) {
+    for (let k = 0; k < clientCount; k++) {
+        if (i == 0 || k == 0 || i == clientCount - 1 || k == clientCount - 1 || i == k) {
+            result += "[*]";
+        } else {
+            result += "   ";
+        }
+    }
+    console.log(result);
+    result = "";
+}
